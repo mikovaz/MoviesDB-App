db19b544c14d261dd2c28a24f96c7f56680057ec
Oscar Corona <oscarmcoronav@gmail.com>
2022-03-23 19:43:13 -0700

    added function

--- buscar.js
+++ buscar.js
@@ -2,3 +2,27 @@ const apiUrl = 'https://api.themoviedb.org/3';
 const apiKey = '4e190b3de4323c16e946a007c06ca059';
 const urlPoster = 'https://image.tmdb.org/t/p/original';
 const apiSearchMovie="https://api.themoviedb.org/3/search/movie?api_key=4e190b3de4323c16e946a007c06ca059&language=es-MX&query=batman&page=1&include_adult=false"
+
+
+
+
+fetch(apiTV).then((respuesta)=> respuesta.json())
+        .then((body)=> {            
+            const peliculas = body.results;
+            console.log(body)
+            peliculas.forEach(pelicula => {
+                const card = `
+                <div class="card col-sm-3 m-3 shadow-lg bg-light" ondblclick="irPelicula('${pelicula.id}')">
+                    <img src="${urlPoster}${pelicula.poster_path}" class="card-img-top mt-3 rounded" alt="${pelicula.original_title}">
+                    <div class="card-body">
+                        <h3>${pelicula.original_name}</h3>
+                        <h6 class="text-muted">Fecha de lanzamiento: ${pelicula.first_air_date}</h6>
+                         <p class="card-text">${pelicula.overview}</p>
+                         <a href="./pelicula.html?id=${pelicula.id}" ><button type="button" class="btn btn-primary" >Detalles</button></a>
+                    </div>
+                </div>
+                `
+                containerCards.insertAdjacentHTML('afterbegin', card)
+            });
+
+        });

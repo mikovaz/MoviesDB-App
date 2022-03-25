0679275e202fd36cf1ddb9c5f96109d1d7c910e4
2112miguel <miguel.moreno.angel@gmail.com>
2022-03-25 16:53:24 -0600

    Delete Fav

--- firebase.js
+++ firebase.js
@@ -1,4 +1,5 @@
 const urlFire="https://peliculas-app-b6b6c-default-rtdb.firebaseio.com/"
+let favGuardado
 
 const crearPeliFav=(peliculaId,backdrop_path,original_title,release_date,overview,funcion)=>{
     const url=`${urlFire}/favoritos.json`
@@ -22,4 +23,54 @@ const recuFav=(funcion)=>{
     .then(respuesta=>respuesta.json())
     .then((body)=>funcion(body))
     .catch((error)=>console.log(error))
+}
+
+const buscarFav= async(id)=>{
+    try{
+        const url=`${urlFire}/favoritos.json`
+        let peliId,data
+        const respoFav= await fetch(url)
+        const body=await respoFav.json()
+        data=JSON.stringify(body)
+        data=JSON.parse(data)
+        const favoritos=Object.entries(data).find((fav)=>fav[1].peliculaId==id)
+        peliId=favoritos[1].peliculaId
+        return peliId
+    }
+    
+    catch(error){console.log(error)}
+}
+
+const borrarFav= async(id)=>{
+    const url=`${urlFire}/favoritos`
+    let favoritoEncontrado
+    try{
+        let peliId,data
+        const respoFav= await fetch(`${url}.json`)
+        const bodyFav=await respoFav.json()
+        data=JSON.stringify(bodyFav)
+        data=JSON.parse(data)
+        const favoritos=Object.entries(data).find((fav)=>fav[1].peliculaId==id)
+        peliId=favoritos[0]
+       // console.log(peliId)
+        const borrarFavorito = await fetch(`${url}/${peliId}.json`,
+        {
+            method: 'DELETE'
+        })
+        const body=await borrarFavorito.json()
+        console.log(body)
+    }   
+    catch(error){
+        console.log(error)
+    }
+    //console.log(favoritoEncontrado)
+    /**
+     * fetch(url).then((respuesta)=>respuesta.json())
+    .then((body)=>{
+        const favoritos=Object.entries(body).find((fav)=>fav[1].peliculaId==id)
+        favoritoEncontrado  =favoritos[0]
+        //delete favoritoEncontrado
+        
+    })
+     */
 }
